11120fce25a79447f86f88eecafb500ba4ee33f5
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-06-21 12:48:37 -0500

    remove unused imports in index view

--- apps/project/page/index/views.py
+++ apps/project/page/index/views.py
@@ -1,14 +1,14 @@
-from django.views.generic import edit
-from dal import autocomplete
-from django_countries import countries
-from .forms import SubscribeNewsletterForm
 from django.urls import reverse_lazy
+from django.views.generic import edit
+
+from .forms import SubscribeNewsletterForm
+
 
 class IndexTemplateView(edit.FormView):
     template_name = "project/page/index/templates/index.html"
     form_class = SubscribeNewsletterForm
     success_url = reverse_lazy('index:home')
-    
+
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
